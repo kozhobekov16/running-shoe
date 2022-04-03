aa73afeda6928aa114465ad6ddebca32e3ad64e8
zhakshylyk <zhakshylykkozhobekov@gmail.com>
2022-04-03 17:09:46 +0600

    all

--- src/pages/chosen/Chosen.jsx
+++ src/pages/chosen/Chosen.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import {NavLink} from 'react-router-dom'
 const Chosen = () => {
     return (
         <div>
+            <div>
+                <NavLink to='/'>
+                    <img src="/images/LeftToBack.svg" alt="left"/>
+                </NavLink>
+                <h1>Мои закладки</h1>
+            </div>
             Chosen
         </div>
     );
