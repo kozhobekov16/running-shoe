5ff10efb00176b3255342eba2be9a2eafd7f9059
zhakshylyk <zhakshylykkozhobekov@gmail.com>
2022-04-03 18:15:50 +0600

    a''

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,43 +4,47 @@ import {NavLink} from 'react-router-dom'
 
 function Header({activeOpenBasket}) {
     return (
-        <div className='flex justify-between items-center'>
-            <div className="flex items-center">
-                <div>
-                    <NavLink to='/'>
-                        <img
-                            src="/images/logo.svg"
-                            alt="logo"
-                            className="mr-2"
-                        />
-                    </NavLink>
+        <div>
+            <div className='flex justify-between items-center'>
+                <div className="flex items-center">
+                    <div>
+                        <NavLink to='/'>
+                            <img
+                                src="/images/logo.svg"
+                                alt="logo"
+                                className="mr-2"
+                            />
+                        </NavLink>
+                    </div>
+                    <div>
+                        <strong className="uppercase">Online Store Sneakers</strong>
+                        <p className="text-slate-800 text-sm">Магазин лучших кроссовок</p>
+                    </div>
                 </div>
-                <div>
-                    <strong className="uppercase">Online Store Sneakers</strong>
-                    <p className="text-slate-800 text-sm">Магазин лучших кроссовок</p>
-                </div>
-            </div>
-            <div className='flex items-center gap-2'>
-                <div className={`flex gap-2 items-center ${styles.basketCard}`}>
+                <div className='flex items-center gap-2'>
+                    <div className={`flex gap-2 items-center ${styles.basketCard}`}>
                     <span>
                         <img onClick={activeOpenBasket} src="/images/basket.svg" alt="basket"/>
                     </span>
-                    <span>
+                        <span>
                         <p className="text-sm">1205 руб.</p>
                     </span>
+                    </div>
+                    <ul className="p-0 flex gap-2">
+                        <li>
+                            <NavLink to='/likes'>
+                                <img src="/images/heartUnLike.svg" alt="heart"/>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <img src="/images/user.svg" alt="user"/>
+                        </li>
+                    </ul>
                 </div>
-                <ul className="p-0 flex gap-2">
-                    <li>
-                        <NavLink to='/likes'>
-                            <img src="/images/heartUnLike.svg" alt="heart"/>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <img src="/images/user.svg" alt="user"/>
-                    </li>
-                </ul>
             </div>
+            <hr className='mt-8'/>
         </div>
+
     )
 }
 
--- src/pages/chosen/Chosen.jsx
+++ src/pages/chosen/Chosen.jsx
@@ -3,11 +3,13 @@ import {NavLink} from 'react-router-dom'
 const Chosen = () => {
     return (
         <div>
-            <div>
+            <div className='flex items-center gap-4 pt-4'>
                 <NavLink to='/'>
-                    <img src="/images/LeftToBack.svg" alt="left"/>
+                    <img
+                        src="/images/LeftToBack.svg"
+                        alt="left"/>
                 </NavLink>
-                <h1>Мои закладки</h1>
+                <h1 className='text-4xl font-bold'>Мои закладки</h1>
             </div>
             Chosen
         </div>
